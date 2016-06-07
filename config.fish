24989ca609b548e7f04353b5f5ea4e6d45a6cff9
Luka Bratos <luka.bratos@gmail.com>
2016-06-07 11:01:34 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -7,9 +7,9 @@ set -gx PATH $homebrew $brew_rbenv $default_path
 set -gx RBENV_ROOT /usr/local/var/rbenv
 set PATH ~/bin $PATH
 
-set -gx BABYLON_WWW /Users/lukabratos/Developer/backbone
-set -gx BABYLON_IOS /Users/lukabratos/Developer/babylon-ios
-set -gx BABYLON_ANDROID /Users/lukabratos/Developer/babylon-android
+set -gx BABYLON_WWW $HOME/Developer/backbone
+set -gx BABYLON_IOS $HOME/Developer/babylon-ios
+set -gx BABYLON_ANDROID $HOME/Developer/babylon-android
 
 function fish_prompt
     set_color $fish_color_cwd
